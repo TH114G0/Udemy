854a5c00509fd015e19eb962f7c524a155a70a56
Thiago Jose <thiago.wilton2704@gmail.com>
2023-10-17 16:01:14 -0300

    html

---
+++ HTML/Exercicios/form/node_9b9fc4e9c2005.tmp.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/usuarios', (req, resp) => {
+    console.log(req.body)
+    resp.send('<h1>Parabéns</h1>')
+})
+
+app.listen(3001, () => {
+    console.log('Servidor em execução na porta 3001')
+})
